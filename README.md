cff2f25c761dad6c6c3b13b9821dc84c8a17687e
Nathan GUIGUI <nathan.guigui@epitech.eu>
2019-09-17 02:49:01 +0200

    :construction: add qt
    add base for qt installation + header + link

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ conan install ..
 cmake .. -G "Unix Makefiles"
 cmake --build .
 ```
+
+#### Installation de Qt (30-35min)
+
+```bash
+conan install qt/5.13.1@bincrafters/stable --build missing
+```
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
+#include <QCoreApplication>
+#include <QObject>
+#include <QString>
+#include <QTimer>
+#include <QtCore/QDebug>
+#include <QtWidgets/QtWidgets>
+#include "main.hpp"
 
-int main() {
-    std::cout << "it Works !\n";
+int main(int argc, char *argv[])
+{
+
+    qDebug() << "toto";
+        // Creates an instance of QApplication
+    QApplication a(argc, argv);
+
+    // This is our MainWidget class containing our GUI and functionality
+    MainWidget w;
+    w.show(); // Show main window
+
+    // run the application and return execs() return value/code
+    return a.exec();
 }
