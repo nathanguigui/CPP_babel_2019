bce7803c4bc89e81a7a81d588a8166c21b51ecb5
lucas poirel <lucas.poirel@epitech.eu>
2019-09-19 16:38:27 +0200

    sound

--- client/src/mainwindow.cpp
+++ client/src/mainwindow.cpp
@@ -21,6 +21,10 @@ MainWindow::MainWindow(QWidget *parent): QWidget(parent)
    mainLayout->addWidget(button_,2,3);
    mainLayout->addWidget(textBrowser_,1,1);
    mainLayout->addWidget(textBox_,1,3);
+   this->setStyleSheet("background-color: rgb(38,38,38);");
+   button_->setStyleSheet("background-color: rgb(255, 255, 255);");
+   textBox_->setStyleSheet("background-color: rgb(255, 255, 255);");
+   textBrowser_->setStyleSheet("background-color: rgb(255, 255, 255);");
    setLayout(mainLayout);
    setWindowTitle(tr("BABEL"));
    connect(button_, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
@@ -39,11 +43,13 @@ QString MainWindow::launchlogin()
    QInputDialog *log = new QInputDialog(this);
    log->setLabelText(tr("Enter login: "));
    log->setWindowTitle(tr("Login"));
-   log->setTextEchoMode(QLineEdit::Password);
    log->adjustSize();
+   log->setStyleSheet("background-color: rgb(255, 255, 255);");
    log->move(QApplication::desktop()->screen()->rect().center() - log->rect().center());
    if (log->exec() == QDialog::Accepted)
       login = log->textValue();
+   else
+      exit(0);
    qDebug() << login;
    return login;
 }
@@ -52,6 +58,9 @@ void MainWindow::launchSplashScreen()
 {
    QMovie *splash = new QMovie("../client/templates/cube.gif");
    QLabel *processLabel = new QLabel(this);
+   //QMediaPlayer *sound = new QMediaPlayer;
+   //sound->setMedia(QUrl::fromLocalFile("../client/templates/launch.mp3");
+   //sound->setVolume(50);
    processLabel->resize(980, 580);
    processLabel->setStyleSheet("background-color: rgb(38,38,38);");
    processLabel->setMovie(splash);
@@ -59,6 +68,7 @@ void MainWindow::launchSplashScreen()
    processLabel->setAlignment(Qt::AlignCenter);
    splash->start();
    processLabel->show();
+   //QTimer::singleShot(3500, sound, SLOT(close()));
    QTimer::singleShot(3500, processLabel, SLOT(close()));
    QTimer::singleShot(0, this, SLOT(show()));
 }
