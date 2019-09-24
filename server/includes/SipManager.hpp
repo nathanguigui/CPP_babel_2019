cc3369b63f86b735a1264786b281c0baf3465509
lucas comte <lcomte@localhost.localdomain>
2019-09-24 08:44:36 +0200

    add Options SIP header

--- server/includes/SipManager.hpp
+++ server/includes/SipManager.hpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <boost/asio.hpp>
+
+
 enum states {
     new_account = 1,
     existing = 2,
@@ -14,6 +17,11 @@ class SipManager {
         ~SipManager();
         void trying_connection(char *request);
         int check_account_existing();
+        int create_account();
+        int update_account();
+        std::stringstream options_header(char *);
+        std::stringstream SipManager::OK_header(char *old_request)
+
     private:
         states is_connected;
         sqlite3 *_database;
--- server/src/SipManager.cpp
+++ server/src/SipManager.cpp
@@ -19,6 +19,68 @@ std::string convertToString(char* a)
     } 
     return s; 
 }
+int check_account_in_db(void *data, int argc, char **argv, char **azColName)
+{
+    if (argc > 0) {
+        return argc;
+    }
+    return 0;
+}
+
+int create_account_in_db(void *data, int argc, char **argv, char **azColName)
+{
+    std::cout << argc << std::endl;
+    return 1;
+}
+
+int update_db_account(void *data, int argc, char **argv, char **azColName)
+{
+    return (0);
+}
+
+int SipManager::update_account()
+{
+    std::string sql_request = "UPDATE user_db SET ip=\"";
+    char *data; 
+    char *zErrMsg = 0;
+
+    sql_request = sql_request + ip;
+    sql_request = sql_request + "\", state=1\n WHERE username=\"";
+    sql_request = sql_request + username;
+    sql_request = sql_request + "\"";
+    sqlite3_exec(_database, sql_request.c_str(), update_db_account, data,&zErrMsg);
+}
+int SipManager::create_account()
+{
+    std::string sql_request;
+    char *data; 
+    char *zErrMsg = 0;
+
+    sql_request = "INSERT into user_db(username, ip, state)\n";
+    sql_request = sql_request + "VALUES (\"";
+    sql_request = sql_request + username;
+    sql_request = sql_request + "\", \"";
+    sql_request = sql_request + ip;
+    sql_request = sql_request + "\", 1)";
+    sqlite3_exec(_database, sql_request.c_str(), create_account_in_db, data,&zErrMsg);
+}
+int SipManager::check_account_existing()
+{
+    std::string sql_request;
+    char *data; 
+    char *zErrMsg = 0;
+
+    sql_request = "SELECT username FROM user_db\nWHERE username == \"";
+    sql_request = sql_request + username;
+    sql_request = sql_request + "\"";
+    if (sqlite3_exec(_database, sql_request.c_str(), check_account_in_db,data, &zErrMsg) != 0) {
+        update_account();
+    } else {
+        create_account();
+    }
+    return (0);
+}
+
 std::string get_IP_in_request(std::string request)
 {
     request.erase(0, request.find("UDP") + 4);
@@ -46,30 +108,6 @@ std::string get_username(std::string request)
     request.erase(request.find("\""), std::string::npos);
     return (request);
 }
-static int callback(void *data, int argc, char **argv, char **azColName){
-   int i;
-   fprintf(stderr, "%s: ", (const char*)data);
-   
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
-
-int SipManager::check_account_existing()
-{
-    std::string sql_request;
-    char *data = "Callback function called"; 
-    char *zErrMsg = 0;
-
-    sql_request = "SELECT username FROM user_db\nWHERE username == \"";
-    sql_request = sql_request + username;
-    sql_request = sql_request + "\"";
-    std::cout << sql_request << std::endl;
-    sqlite3_exec(_database, sql_request.c_str(), callback,data, &zErrMsg);
-    return (0);
-}
 
 void SipManager::trying_connection(char *request)
 {
@@ -80,6 +118,52 @@ void SipManager::trying_connection(char *request)
     username = get_username(test);
     ss << ip << "\nFrom: " << get_user_in_request(test, 1) << "To: " << get_user_in_request(test, 0);
     ss << "\n" << get_callID(test) << "User-Agent: Asterisk PBX\n" << "Allow: INVITE, ACK, CANCEL, OPTIONS, BYE, REFER, SUBSCRIBE, NOTIFY\nSupported: replaces\nContact: " << get_user_in_request(test, 0).erase(0, username.size() + 2) << "\nContent-Length: 0\n";
-    std::cout << ss.str();
     check_account_existing();
+    options_header(request);
+}
+std::string get_IP_from_iface()
+{
+char hostbuffer[256]; 
+    char *IPbuffer; 
+    struct hostent *host_entry; 
+    int hostname; 
+  
+    // To retrieve hostname 
+    hostname = gethostname(hostbuffer, sizeof(hostbuffer)); 
+  
+    // To retrieve host information 
+    host_entry = gethostbyname(hostbuffer); 
+  
+    // To convert an Internet network 
+    // address into ASCII string 
+    IPbuffer = inet_ntoa(*((struct in_addr*) 
+                           host_entry->h_addr_list[0])); 
+    return convertToString(IPbuffer); 
+}
+
+std::string make_daytime_string()
+{
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
+
+std::stringstream SipManager::options_header(char *old_request)
+{
+    std::string request = convertToString(old_request);
+    auto ss = std::stringstream();
+    ss << "OPTIONS sip:1010@" << get_IP_from_iface() << ":25565;SIP/2.0";
+    ss << "\nVia: SIP/2.0/UDP " << ip << "\nFrom:" << get_user_in_request(request, 1);
+    ss << "To: <sip:1010@" << ip << "\nContact: <sip:"  << username << "@" << ip << ">";
+    ss << "Call-ID: \n" << "CSeq: 102 OPTIONS\nUser-Agent:" << username << "\nMax-Forwards: 70\nDate: ";
+    ss << make_daytime_string() << "Allow: INVITE, ACK, CANCEL, OPTIONS, BYE, REFER, SUBSCRIBE, NOTIFY\n";
+    ss << "Supported: replaces\nContent-Length: 0";
+    std::cout << request << std::endl;
+    std::cout << ss.str() << std::endl;
+    return ss;
+}
+
+std::stringstream SipManager::OK_header(char *old_request)
+{
+    
 }
