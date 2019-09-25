4a8100b40ecc0faa6c38f5637622942ae22acd26
Nathan GUIGUI <nathan.guigui@epitech.eu>
2019-09-25 20:43:05 +0200

    :heavy_plus_sign: add portaudio and opus codec
    add portaudio and opus codex

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,9 @@ class CPP_Babel(ConanFile):
    name = "client"
    version = "0.1"
    settings = "os_build", "compiler", "build_type", "arch"
-   requires = "boost/1.71.0@conan/stable", "qt/5.13.1@bincrafters/stable" # comma-separated list of requirements
+   requires = "boost/1.71.0@conan/stable", "qt/5.13.1@bincrafters/stable", "portaudio/v190600.20161030@bincrafters/stable", "opus/1.3.1@bincrafters/stable" # comma-separated list of requirements
    generators = "cmake", "gcc", "txt", "cmake_find_package"
-   build_policy = "always"
+   build_policy = "missing"
 
    def build(self):
       cmake = CMake(self)
